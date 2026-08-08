3c2db4d9a41304f7da321e4708692b051ac9dab9
msaad <msaad23305@gmail.com>
2026-08-09 01:59:04 +0500

    Delete all python scripts and align category names to top left

--- convert_new_logo.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import pillow_avif
-import os
-
-img = Image.open('assets/images/Logo.png')
-img.save('assets/images/logo.avif', 'AVIF')
-print("Converted Logo.png to logo.avif")
